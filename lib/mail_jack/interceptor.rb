948d497cc229d4b684a15dae62812f1dbe43b959
Peter P <pete@p373.net>
2013-06-17 23:49:47 -0700

    fixed issue with adding tracking codes with multipart emails that have attachments

--- lib/mail_jack/interceptor.rb
+++ lib/mail_jack/interceptor.rb
@@ -1,20 +1,38 @@
 module MailJack
   class Interceptor
     def self.delivering_email(message)
-      body = message.body.to_s
-      hrefs = body.scan(/href=\"(.*?)\"/).flatten
-      hrefs = hrefs.grep(MailJack.config.href_filter)
-      hrefs = hrefs.uniq
+      querystr = generate_query_string(message)
 
+      if message.multipart?
+        message.parts.each do |part|
+          unless part.attachment?
+            part.body = append_links(part.body, querystr)
+          end
+        end
+      else
+        message.body = append_links(message.body, querystr)
+      end
+    end
+
+    def self.generate_query_string(message)
       querystr = MailJack.trackables.keys.inject({}) {|hash, a| hash[a] = message.send(a); hash}.to_query
       if MailJack.config.enable_encoding?
         querystr = "#{MailJack.config.encode_to}=#{Base64.encode64(querystr)}"
       end
+      return querystr
+    end
+
+    def self.append_links(text, querystr)
+      str = text.to_s
+      hrefs = str.scan(/href=\"(.*?)\"/).flatten
+      hrefs = hrefs.grep(MailJack.config.href_filter)
+      hrefs = hrefs.uniq
+
       hrefs.each do |h| 
         q = h.include?("?") ?  "&#{querystr}" : "?#{querystr}" 
-        body.gsub!(h, "#{h}#{q}")
+        str.gsub!(h, "#{h}#{q}")
       end
-      message.body = body
+      return str
     end
   end
 end
--- lib/mail_jack/version.rb
+++ lib/mail_jack/version.rb
@@ -1,4 +1,4 @@
 module MailJack
   # The version of the gem
-  VERSION = '0.2.3'.freeze unless defined?(::MailJack::VERSION)
+  VERSION = '0.2.4'.freeze unless defined?(::MailJack::VERSION)
 end
